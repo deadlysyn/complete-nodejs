ba0ef7899d86c1ca38241f824a9deb458102e85d
Mike Hoskins <mrh@deadlysyn.com>
2019-06-02 22:33:32 -0400

    use a third-party package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
-const getNotes = require("./notes.js");
+const getNotes = require('./notes.js')
+const validator = require('validator')
 
-const msg = getNotes();
+const msg = getNotes()
+console.log(msg)
 
-console.log(msg);
+console.log(validator.isEmail('@gmail.com'))
+console.log(validator.isURL('google.com'))
