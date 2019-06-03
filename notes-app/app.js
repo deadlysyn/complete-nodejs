d35e3a79753d725fd8132a50da617c8030c1a318
Mike Hoskins <mrh@deadlysyn.com>
2019-06-02 22:44:19 -0400

    print to console in color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,11 @@
 const getNotes = require('./notes.js')
 const validator = require('validator')
+const chalk = require('chalk')
 
 const msg = getNotes()
 console.log(msg)
 
 console.log(validator.isEmail('@gmail.com'))
 console.log(validator.isURL('google.com'))
+
+console.log(chalk.bold.inverse.green('Success!'))
