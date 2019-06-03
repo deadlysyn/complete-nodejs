5bc5d81616ddb715ac5fee452dd278ae8bfdf4f2
Mike Hoskins <mhoskins@corelogic.com>
2019-06-02 21:16:08 -0400

    append vs write notes file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,6 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
+const notesFile = 'notes.txt';
+
+// fs.writeFileSync(notesFile, 'This file was created by Node.js!');
+fs.appendFileSync(notesFile, 'This file was appended by Node.js!');
