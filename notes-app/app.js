5741cc8658a02ee4dec46b00b0b31f2735467793
Mike Hoskins <mrh@deadlysyn.com>
2019-06-17 23:11:25 -0400

    add argument parsing

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,59 @@
-//const chalk = require('chalk')
-//const getNotes = require('./notes.js')
+const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
 
-const command = process.argv[2]
+const command = process.argv[2];
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+// Customize yargs version
+yargs.version('1.0.1');
 
-console.log(process.argv)
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    console.log(`Title: ${argv.title}`);
+    console.log(`Body: ${argv.body}`);
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler() {
+    console.log('Removing note');
+  },
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler() {
+    console.log('Reading note');
+  },
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List notes',
+  handler() {
+    console.log('Listing notes');
+  },
+});
+
+yargs.parse();
