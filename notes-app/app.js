3bba3995cb19d73550401fc82f373a8512b46e40
Mike Hoskins <mrh@deadlysyn.com>
2019-06-18 21:23:36 -0400

    linting to remove semis

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,11 @@
-const chalk = require('chalk');
-const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
-const command = process.argv[2];
+const command = process.argv[2]
 
 // Customize yargs version
-yargs.version('1.0.1');
+yargs.version('1.0.1')
 
 // Create add command
 yargs.command({
@@ -24,36 +24,35 @@ yargs.command({
     },
   },
   handler(argv) {
-    console.log(`Title: ${argv.title}`);
-    console.log(`Body: ${argv.body}`);
+    notes.addNote(argv.title, argv.body)
   },
-});
+})
 
 // Create remove command
 yargs.command({
   command: 'remove',
   describe: 'Remove a note',
   handler() {
-    console.log('Removing note');
+    console.log('Removing note')
   },
-});
+})
 
 // Create read command
 yargs.command({
   command: 'read',
   describe: 'Read a note',
   handler() {
-    console.log('Reading note');
+    console.log('Reading note')
   },
-});
+})
 
 // Create list command
 yargs.command({
   command: 'list',
   describe: 'List notes',
   handler() {
-    console.log('Listing notes');
+    console.log('Listing notes')
   },
-});
+})
 
-yargs.parse();
+yargs.parse()
