6ca704624feb51e51b4e801bf976b1bfe9ffa254
Mike Hoskins <mrh@deadlysyn.com>
2019-06-30 23:38:23 -0400

    refactor geocode function as module

---
+++ weather-app/util/geocode.js
@@ -0,0 +1,25 @@
+const request = require('request')
+
+const mapEndpoint = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
+const mapOptions = '?limit=1&access_token='
+const mapToken = process.env.MAP_TOKEN
+
+const geocode = (address, callback) => {
+  const mapLocation = `${encodeURIComponent(address)}.json`
+  const mapURL = mapEndpoint + mapLocation + mapOptions + mapToken
+  request({ url: mapURL, json: true }, (error, response, body) => {
+    if (error) {
+      callback(Error('Unable to reach mapping service.'))
+    } else if (body.features.length === 0) {
+      callback(Error('Unable to find location.'))
+    } else {
+      callback(undefined, {
+        longitude: `${body.features[0].center[0]}`,
+        latitude: `${body.features[0].center[1]}`,
+        name: `${body.features[0].place_name}`,
+      })
+    }
+  })
+}
+
+module.exports = geocode
