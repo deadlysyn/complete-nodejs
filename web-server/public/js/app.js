5924db436ce02fafbd6ba10be220aa4b76e71b58
Mike Hoskins <mrh@deadlysyn.com>
2019-07-05 01:10:14 -0400

    wire up ui and tweak styles

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,15 +1,23 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', e => {
   e.preventDefault()
 
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+
   fetch(`/weather?address=${search.value}`).then(res => {
     res.json().then(({ error, address, forecast, location } = {}) => {
       if (error) {
-        return console.log(error)
+        messageOne.textContent = 'Error retrieving forecast:'
+        messageTwo.textContent = error
+      } else {
+        messageOne.textContent = `Forecast for ${location}:`
+        messageTwo.textContent = forecast
       }
-      console.log(`${location}: ${forecast}`)
     })
   })
 })
