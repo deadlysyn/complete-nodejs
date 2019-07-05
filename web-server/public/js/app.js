0e1c81ffedba605516670f041193cc894168e593
Mike Hoskins <mrh@deadlysyn.com>
2019-07-05 00:22:54 -0400

    client side fetch to api

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,10 @@
 console.log('app.js loaded')
+
+fetch('/weather?address=boston').then(res => {
+  res.json().then(({ error, address, forecast, location } = {}) => {
+    if (error) {
+      return console.log(error)
+    }
+    console.log(`${location}: ${forecast}`)
+  })
+})
