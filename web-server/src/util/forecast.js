d8de8be56f5f093f69f3dc0597adb82461f86b0f
Mike Hoskins <mrh@deadlysyn.com>
2019-07-07 23:55:11 -0400

    material theme; tweak forecast

--- web-server/src/util/forecast.js
+++ web-server/src/util/forecast.js
@@ -11,10 +11,9 @@ const forecast = (longitude, latitude, callback) => {
     } else if (body.error) {
       callback(Error('Unable to find location.'))
     } else {
-      console.log(url)
       callback(undefined, {
         summary: `${body.daily.data[0].summary}`,
-        temp: `It is currently ${body.currently.temperature} degress out.`,
+        temp: `It is currently ${body.currently.temperature} degress out. Today's high will be ${body.daily.data[0].temperatureHigh}, with a low of ${body.daily.data[0].temperatureLow} degrees.`,
         percip: `${body.currently.humidity * 100}% humidity with a ${
           body.currently.precipProbability
         }% chance of rain.`,
