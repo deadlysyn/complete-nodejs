28726c85964b3966fe4057626ce03db8f1ce2739
Mike Hoskins <mrh@deadlysyn.com>
2019-09-09 23:25:29 -0400

    promise chaining

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,12 +1,29 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve('This is success!') // success
-    reject('This is my error!') // error
-  }, 2000)
-})
+// const doWorkPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve('This is success!') // success
+//     reject('This is my error!') // error
+//   }, 2000)
+// })
 
-doWorkPromise.then((result) => {
-  console.log(result)
-}).catch((error) => {
-  console.log(error)
+// doWorkPromise.then((result) => {
+//   console.log(result)
+// }).catch((error) => {
+//   console.log(error)
+// })
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b)
+    }, 2000)
+  })
+}
+
+add(1, 1).then((sum) => {
+  console.log(sum)
+  return add(sum, 4)
+}).then((sum2) => {
+  console.log(sum2)
+}).catch((e) => {
+  console.log(e.message)
 })
