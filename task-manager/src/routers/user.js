9d0038c44ff5e0fd36a489608ceaacc8f3673139
Mike Hoskins <mrh@deadlysyn.com>
2019-09-22 22:23:30 -0400

    save avatars in mongodb

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -80,7 +80,7 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-  dest: 'avatars',
+  // dest: 'avatars',
   limits: {
     fileSize: 1024000 // 1MB in bytes
   },
@@ -92,7 +92,9 @@ const upload = multer({
   }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+  req.user.avatar = req.file.buffer
+  await req.user.save()
   res.send()
 }, (error, req, res, next) => {
   res.status(400).send({ error: error.message})
