e4deece3225f7e2977660a06973994c394d20979
Mike Hoskins <mrh@deadlysyn.com>
2019-09-29 22:28:26 -0400

    bootstrap socket.io

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,12 +1,30 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
+// socket.io must be passed raw http server
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000
 const documentRoot = path.join(__dirname, '../public')
 
 app.use(express.static(documentRoot))
 
-app.listen(port, () => {
+let count = 0
+
+io.on('connection', (socket) => {
+  console.log('new websocket connection')
+  socket.emit('countUpdated', count)
+  socket.on('increment', () => {
+    count++
+    // socket.emit('countUpdated', count) // one client
+    io.emit('countUpdated', count) // all clients
+  })
+})
+
+server.listen(port, () => {
   console.log(`listening on port ${port}...`)
 })
