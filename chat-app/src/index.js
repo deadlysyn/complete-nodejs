924d6b0585ae4567e4f84893b0ca0b0e9f85bfec
Mike Hoskins <mrh@deadlysyn.com>
2019-10-02 20:12:09 -0400

    add timestamps to messages

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 // socket.io must be passed raw http server
 const app = express()
@@ -17,25 +18,25 @@ app.use(express.static(documentRoot))
 io.on('connection', (socket) => {
   console.log('new websocket connection')
 
-  socket.emit('message', 'Welcome!')
-  socket.broadcast.emit('message', 'A new user has joined...')
+  socket.emit('message', generateMessage('Welcome!'))
+  socket.broadcast.emit('message', generateMessage('A new user has joined...'))
 
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter()
     if (filter.isProfane(message)) {
       return callback('Profanity detected!')
     }
-    io.emit('message', message)
+    io.emit('message', generateMessage(message))
     callback()
   })
 
   socket.on('sendLocation', (coords, callback) => {
-    io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
     callback()
   })
 
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left.')
+    io.emit('message', generateMessage('A user has left.'))
   })
 })
 
